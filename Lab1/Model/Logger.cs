50fe1160861c5e957af05639ee4fa760cc240f1f
Niklas Andren <niklas.andren@hotmail.com>
2012-09-02 14:26:06 +0200

    Uppgift 7

---
+++ Lab1/Model/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1.Model
+{
+    class Logger
+    {
+
+        private List<string> LogList = new List<string>();
+
+        public void Log(string Msg)
+        {
+            if (LogList.Count > 10)
+            {
+                LogList.RemoveAt(0);
+                LogList.Add(Msg);
+            }
+            else
+            {
+                LogList.Add(Msg);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}\n", LogList);
+        }
+
+    }
+}
